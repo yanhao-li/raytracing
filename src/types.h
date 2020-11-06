b533066086bce25efa5fbbc95e2d8920bb284bee
Yanhao Li <hi@yanhaoli.com>
2020-11-06 11:15:40 -0500

    refactor: types

---
+++ src/types.h
@@ -0,0 +1,111 @@
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+using namespace Eigen;
+
+////////////////////////////////////////////////////////////////////////////////
+// Define types & classes
+////////////////////////////////////////////////////////////////////////////////
+
+struct Ray {
+	Vector3d origin;
+	Vector3d direction;
+	Ray() { }
+	Ray(Vector3d o, Vector3d d) : origin(o), direction(d) { }
+};
+
+struct Light {
+	Vector3d position;
+	Vector3d intensity;
+};
+
+struct Intersection {
+	Vector3d position;
+	Vector3d normal;
+	double ray_param;
+};
+
+struct Camera {
+	bool is_perspective;
+	Vector3d position;
+	double field_of_view; // between 0 and PI
+	double focal_length;
+	double lens_radius; // for depth of field
+};
+
+struct Material {
+	Vector3d ambient_color;
+	Vector3d diffuse_color;
+	Vector3d specular_color;
+	double specular_exponent; // Also called "shininess"
+
+	Vector3d reflection_color;
+	Vector3d refraction_color;
+	double refraction_index;
+};
+
+struct Object {
+	Material material;
+	virtual ~Object() = default; // Classes with virtual methods should have a virtual destructor!
+	virtual bool intersect(const Ray &ray, Intersection &hit) = 0;
+};
+
+// We use smart pointers to hold objects as this is a virtual class
+typedef std::shared_ptr<Object> ObjectPtr;
+
+struct Sphere : public Object {
+	Vector3d position;
+	double radius;
+
+	virtual ~Sphere() = default;
+	virtual bool intersect(const Ray &ray, Intersection &hit) override;
+};
+
+struct Parallelogram : public Object {
+	Vector3d origin;
+	Vector3d u;
+	Vector3d v;
+
+	virtual ~Parallelogram() = default;
+	virtual bool intersect(const Ray &ray, Intersection &hit) override;
+};
+
+struct AABBTree {
+	struct Node {
+		AlignedBox3d bbox;
+		int parent; // Index of the parent node (-1 for root)
+		int left; // Index of the left child (-1 for a leaf)
+		int right; // Index of the right child (-1 for a leaf)
+		int triangle; // Index of the node triangle (-1 for internal nodes)
+	};
+
+	std::vector<Node> nodes;
+	int root;
+
+	AABBTree() = default; // Default empty constructor
+	AABBTree(const MatrixXd &V, const MatrixXi &F); // Build a BVH from an existing mesh
+};
+
+struct Mesh : public Object {
+	MatrixXd vertices; // n x 3 matrix (n points)
+	MatrixXi facets; // m x 3 matrix (m triangles)
+
+	AABBTree bvh;
+
+	Mesh() = default; // Default empty constructor
+	Mesh(const std::string &filename);
+	virtual ~Mesh() = default;
+	virtual bool intersect(const Ray &ray, Intersection &hit) override;
+};
+
+struct Scene {
+	Vector3d background_color;
+	Vector3d ambient_light;
+
+	Camera camera;
+	std::vector<Material> materials;
+	std::vector<Light> lights;
+	std::vector<ObjectPtr> objects;
+};
+
+////////////////////////////////////////////////////////////////////////////////
